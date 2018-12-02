7380e1a446b6e5dd2d1ef35a38fa90899d03e010
Michael Herwig <michael.herwig@rwth-aachen.de>
2018-12-02 23:19:38 +0100

    [BM] [CNX] benchmarks cnx with alterating heights
    The benchmark will test the hashrate of 10 randomly generated block blobs at different height. This is useful to detect bottlenecks argue about hr and verifying a consistent hr on the ss window (at least for the hardware testing on)

---
+++ tests/benchmark/BM_CryptoNightX.cpp
@@ -0,0 +1,23 @@
+﻿#include <benchmark/benchmark.h>
+
+#include "HashBasedBenchmark.h"
+#include "crypto/cnx/cnx.h"
+
+using HashAlgorithm = Crypto::CNX::Hash_v0;
+
+static void HeightArguments(benchmark::internal::Benchmark* b) {
+  for (uint32_t i = 0; i <= 2 * HashAlgorithm::windowSize(); i += 4)
+      b->Args({i});
+}
+
+BENCHMARK_DEFINE_F(HashBasedBenchmark, BM_CryptoNightX)(benchmark::State& state) {
+  unsigned char const * data = HashBasedBenchmark::data();
+  uint32_t height = static_cast<uint32_t>(state.range(0));
+  for (auto _ : state)
+  {
+    for(std::size_t i = 0; i < BlockCount; ++i)
+      HashAlgorithm{}(data + i * BlockSize, BlockSize, HashPlaceholder, height);
+  }
+}
+
+BENCHMARK_REGISTER_F(HashBasedBenchmark, BM_CryptoNightX)->Apply(HeightArguments)->Unit(benchmark::kMillisecond);
