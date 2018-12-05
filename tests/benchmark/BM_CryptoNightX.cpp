65fe607dbb1ac00fcc99527fa7a681b5916cd525
Michael Herwig <michael.herwig@rwth-aachen.de>
2018-12-05 19:56:10 +0100

    [BM] added height counter

--- tests/benchmark/BM_CryptoNightX.cpp
+++ tests/benchmark/BM_CryptoNightX.cpp
@@ -6,15 +6,17 @@
 using HashAlgorithm = Crypto::CNX::Hash_v0;
 
 static void HeightArguments(benchmark::internal::Benchmark* b) {
-  for (uint32_t i = 0; i <= 2 * HashAlgorithm::windowSize(); i += 4)
+  for (uint32_t i = 0; i <= 2 * HashAlgorithm::windowSize(); i += 1)
       b->Args({i});
 }
 
 BENCHMARK_DEFINE_F(HashBasedBenchmark, BM_CryptoNightX)(benchmark::State& state) {
   unsigned char const * data = HashBasedBenchmark::data();
   uint32_t height = static_cast<uint32_t>(state.range(0));
+  state.counters["Height"] = height;
   for (auto _ : state)
   {
+    (void)_;
     for(std::size_t i = 0; i < BlockCount; ++i)
       HashAlgorithm{}(data + i * BlockSize, BlockSize, HashPlaceholder, height);
   }
