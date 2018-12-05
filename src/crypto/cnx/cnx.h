48dc24dc4a2197561b2c81d1e524c0abc88c7594
Michael Herwig <michael.herwig@rwth-aachen.de>
2018-12-05 19:58:11 +0100

    [CNX] Configuration Definition
    Moved the CN-A and SS definition into a separated struct to inherit from. Further the templated configuration checks some issues that may could occur with faulty configuration parameters chosen.

--- src/crypto/cnx/cnx.h
+++ src/crypto/cnx/cnx.h
@@ -1,22 +1,21 @@
 ﻿#pragma once
 
 #include "crypto/hash.h"
+#include "crypto/cnx/configuration.h"
 
 namespace Crypto {
 namespace CNX {
 
-struct Hash_v0 {
-  static constexpr int variant() { return 1; }
-  static constexpr uint32_t windowSize() { return 128; }
-
-  static constexpr uint32_t minRandomizerSize() { return 1 << 5; }
-  static constexpr uint32_t maxRandomizerSize() { return 1 << 15; }
-  static constexpr uint32_t slopeRandomizerSize() { return (maxRandomizerSize() - minRandomizerSize()) / windowSize(); }
-
-  static constexpr uint32_t minScratchpadSize() { return 1 * 1024 * 1024; }
-  static constexpr uint32_t maxScratchpadSize() { return 2 * 1024 * 1024; }
-  static constexpr uint32_t slopeScratchpadSize() { return (maxScratchpadSize() - minScratchpadSize()) / windowSize(); }
-
+struct Hash_v0
+    : Configuration
+      <
+        2048,
+        2 * 1024,
+        4 * 1024,
+        1 * 256 * 1024,
+        2 * 256 * 1024
+      >
+{
   void operator()(const void *data, size_t length, Hash &hash, uint32_t height) const;
 };
 
