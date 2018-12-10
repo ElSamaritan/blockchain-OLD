913d6a92a7b5879ee02469b46f70159dddc5bb41
Michael Herwig <michael.herwig@rwth-aachen.de>
2018-12-10 09:21:14 +0100

    fixed config reformatting

--- src/Miner/MiningConfig.cpp
+++ src/Miner/MiningConfig.cpp
@@ -72,34 +72,28 @@ MiningConfig::MiningConfig() : help(false), version(false) {}
 void MiningConfig::parse(int argc, char** argv) {
   cxxopts::Options options(argv[0], CommonCLI::header());
 
-  options.add_options("Core")("help", "Display this help message", cxxopts::value<bool>(help)->implicit_value("true"))(
-      "version", "Output software version information",
-      cxxopts::value<bool>(version)->default_value("false")->implicit_value("true"));
+  // clang-format off
+  options.add_options("Core")
+    ("help", "Display this help message", cxxopts::value<bool>(help)->implicit_value("true"))
+    ("version", "Output software version information", cxxopts::value<bool>(version)->default_value("false")->implicit_value("true"));
 
   std::string daemonAddress;
-  options.add_options("Daemon")("daemon-address",
-                                "The daemon [host:port] combination to use for node operations. This option overrides "
-                                "--daemon-host and --daemon-rpc-port",
-                                cxxopts::value<std::string>(daemonAddress), "<host:port>")(
-      "daemon-host", "The daemon host to use for node operations",
-      cxxopts::value<std::string>(daemonHost)->default_value("127.0.0.1"),
-      "<host>")("daemon-rpc-port", "The daemon RPC port to use for node operations",
-                cxxopts::value<uint16_t>(daemonPort)->default_value(std::to_string(CryptoNote::RPC_DEFAULT_PORT)), "#")(
-      "scan-time", "Blockchain polling interval (seconds). How often miner will check the Blockchain for updates",
-      cxxopts::value<size_t>(scanPeriod)->default_value("30"), "#");
+  options.add_options("Daemon")
+    ("daemon-address", "The daemon [host:port] combination to use for node operations. This option overrides --daemon-host and --daemon-rpc-port",
+      cxxopts::value<std::string>(daemonAddress), "<host:port>")
+    ("daemon-host", "The daemon host to use for node operations", cxxopts::value<std::string>(daemonHost)->default_value("127.0.0.1"), "<host>")
+    ("daemon-rpc-port", "The daemon RPC port to use for node operations", cxxopts::value<uint16_t>(daemonPort)->default_value(std::to_string(CryptoNote::RPC_DEFAULT_PORT)), "#")
+    ("scan-time", "Blockchain polling interval (seconds). How often miner will check the Blockchain for updates", cxxopts::value<size_t>(scanPeriod)->default_value("30"), "#");
 
-  options.add_options("Mining")("address", "The valid CryptoNote miner's address",
-                                cxxopts::value<std::string>(miningAddress), "<address>")(
-      "block-timestamp-interval",
-      "Timestamp incremental step for each subsequent block. May be set only if --first-block-timestamp has been set.",
-      cxxopts::value<int64_t>(blockTimestampInterval)->default_value("0"),
-      "#")("first-block-timestamp", "Set timestamp to the first mined block. 0 means leave timestamp unchanged",
-           cxxopts::value<uint64_t>(firstBlockTimestamp)->default_value("0"),
-           "#")("limit", "Mine this exact quantity of blocks and then stop. 0 means no limit",
-                cxxopts::value<size_t>(blocksLimit)->default_value("0"), "#")(
-      "log-level", "Specify log level. Must be 0 - 5", cxxopts::value<uint8_t>(logLevel)->default_value("3"), "#")(
-      "threads", "The mining threads count. Must not exceed hardware capabilities.",
-      cxxopts::value<size_t>(threadCount)->default_value(std::to_string(CONCURRENCY_LEVEL)), "#");
+  options.add_options("Mining")
+    ("address", "The valid CryptoNote miner's address", cxxopts::value<std::string>(miningAddress), "<address>")
+    ("block-timestamp-interval", "Timestamp incremental step for each subsequent block. May be set only if --first-block-timestamp has been set.",
+      cxxopts::value<int64_t>(blockTimestampInterval) ->default_value("0"), "#")
+    ("first-block-timestamp", "Set timestamp to the first mined block. 0 means leave timestamp unchanged", cxxopts::value<uint64_t>(firstBlockTimestamp)->default_value("0"), "#")
+    ("limit", "Mine this exact quantity of blocks and then stop. 0 means no limit", cxxopts::value<size_t>(blocksLimit)->default_value("0"), "#")
+    ("log-level", "Specify log level. Must be 0 - 5", cxxopts::value<uint8_t>(logLevel)->default_value("3"), "#")
+    ("threads", "The mining threads count. Must not exceed hardware capabilities.", cxxopts::value<size_t>(threadCount)->default_value(std::to_string(CONCURRENCY_LEVEL)), "#");
+  // clang-format on
 
   try {
     auto result = options.parse(argc, argv);
