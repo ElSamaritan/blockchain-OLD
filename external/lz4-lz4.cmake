26b50bf3a84b438855264a2c19da7a4193c3d48b
Michael Herwig <michael.herwig@rwth-aachen.de>
2018-12-14 10:59:36 +0100

    [EXTERNAL] added lz4 dependency

---
+++ external/lz4-lz4.cmake
@@ -0,0 +1,29 @@
+﻿if(CMAKE_BUILD_TYPE STREQUAL "Debug")
+  set(LZ4_BUILD_TYPE "Debug")
+else()
+  set(LZ4_BUILD_TYPE "Release")
+endif()
+
+ExternalProject_Add(
+  lz4-lz4
+
+  PREFIX "lz4-lz4"
+
+  UPDATE_COMMAND ""
+  PATCH_COMMAND ""
+  TEST_COMMAND ""
+
+  SOURCE_DIR "${CMAKE_CURRENT_SOURCE_DIR}/lz4-lz4"
+  INSTALL_DIR ${CMAKE_CURRENT_BINARY_DIR}/lz4
+
+  CMAKE_ARGS
+    -DCMAKE_INSTALL_PREFIX:PATH=${CMAKE_CURRENT_BINARY_DIR}/lz4-install
+    -DCMAKE_BUILD_TYPE=${LZ4_BUILD_TYPE}
+)
+ExternalProject_Get_Property(lz4-lz4 INSTALL_DIR)
+
+add_library(lz4 STATIC IMPORTED GLOBAL)
+add_dependencies(lz4 lz4-lz4)
+
+set_property(TARGET lz4 PROPERTY IMPORTED_LOCATION ${INSTALL_DIR}/lib/lz4${CMAKE_STATIC_LIBRARY_SUFFIX})
+set_property(TARGET lz4 APPEND PROPERTY INTERFACE_INCLUDE_DIRECTORIES "${INSTALL_DIR}/include")
