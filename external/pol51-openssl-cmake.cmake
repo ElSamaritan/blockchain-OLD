1d81d52f69fa962d56dd2f15566f6f5510fe582f
Michael Herwig <michael.herwig@rwth-aachen.de>
2018-12-15 01:15:15 +0100

    [DEPS] added OpenSSL as external dep if not provided

---
+++ external/pol51-openssl-cmake.cmake
@@ -0,0 +1,15 @@
+﻿## OpenSSL -- HTTPS Support
+find_package(OpenSSL QUIET)
+if(OPENSSL_FOUND)
+  message(STATUS "[--XI--] Found OpenSSL Version ${OEPNSSL_VERSION}. You will support HTTPS encryption.")
+else() # !OPENSSL_FOUND
+  message(STATUS "[--XI--] Could not find any preinstalled OpenSSL Version. Going to include the openssl"
+    " verison shipped with the buildsystem.")
+  set(_CMAKE_C_FLAGS ${CMAKE_C_FLAGS})
+  if(MSVC)
+    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} /w")
+  endif()
+  set(BUILD_SHARED_LIBS OFF)
+  add_subdirectory(pol51-openssl-cmake)
+  set(CMAKE_C_FLAGS ${_CMAKE_C_FLAGS})
+endif() # OPENSSL_FOUND
