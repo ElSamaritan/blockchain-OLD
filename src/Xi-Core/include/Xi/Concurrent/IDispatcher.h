c940f0bf96d3a0206dadbae3e7e3a7ba72fd25cd
Michael Herwig <michael.herwig@rwth-aachen.de>
2018-12-21 11:53:51 +0100

    [DOC] dispatcher documentation

--- src/Xi-Core/include/Xi/Concurrent/IDispatcher.h
+++ src/Xi-Core/include/Xi/Concurrent/IDispatcher.h
@@ -4,10 +4,18 @@
 
 namespace Xi {
 namespace Concurrent {
+/*!
+ * \brief The IDispatcher class abstract the implementation of a synchronization schema to queue tasks for a single
+ * thread.
+ */
 class IDispatcher {
  public:
   virtual ~IDispatcher() = default;
 
+  /*!
+   * \brief post pushes a new working task to the queue
+   * \param cn the task to execute
+   */
   virtual void post(std::function<void()> cn) = 0;
 };
 }  // namespace Concurrent
--- src/Xi-Core/include/Xi/Concurrent/SystemDispatcher.h
+++ src/Xi-Core/include/Xi/Concurrent/SystemDispatcher.h
@@ -6,11 +6,23 @@
 
 namespace Xi {
 namespace Concurrent {
+/*!
+ * \brief The SystemDispatcher class implements a thread working queue based on the system library dispatcher
+ * implementation
+ */
 class SystemDispatcher : public IDispatcher {
  public:
+  /*!
+   * \brief SystemDispatcher wraps a system library dispatcher
+   * \param dispatcher the system library dispatcher to wrap
+   */
   SystemDispatcher(System::Dispatcher& dispatcher);
   ~SystemDispatcher() override = default;
 
+  /*!
+   * \brief post enqueues the task to the system library dispatcher
+   * \param cn the task to execute
+   */
   void post(std::function<void()> cn) override;
 
  private:
