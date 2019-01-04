4e9da9e14a8a973ee2d8c087b3cfc57660ab31a2
Michael Herwig <michael.herwig@rwth-aachen.de>
2019-01-04 15:53:17 +0100

    [CRYPTO] replaced MersenneTwister implementation

--- src/Xi-Crypto/include/Xi/Crypto/MersenneTwister.h
+++ src/Xi-Crypto/include/Xi/Crypto/MersenneTwister.h
@@ -22,6 +22,7 @@
  * ============================================================================================== */
 
 #include <cinttypes>
+#include <memory>
 
 namespace Xi {
 namespace Crypto {
@@ -30,78 +31,25 @@ namespace Crypto {
  * platforms.
  */
 class MersenneTwister {
- private:
-  static const uint32_t N = 624;
-  static const uint32_t M = 397;
-  static const uint32_t MATRIX_A = 0x9908b0df;
-  static const uint32_t UPPER_MASK = 0x80000000;
-  static const uint32_t LOWER_MASK = 0x7fffffff;
-  static const uint32_t TEMPERING_MASK_B = 0x9d2c5680;
-  static const uint32_t TEMPERING_MASK_C = 0xefc60000;
-
-  const uint32_t mag01[2] = {0x0, MATRIX_A};
-
-  inline uint32_t TEMPERING_SHIFT_U(uint32_t y) { return (y >> 11); }
-
-  inline uint32_t TEMPERING_SHIFT_S(uint32_t y) { return (y << 7); }
-
-  inline uint32_t TEMPERING_SHIFT_T(uint32_t y) { return (y << 15); }
-
-  inline uint32_t TEMPERING_SHIFT_L(uint32_t y) { return (y >> 18); }
-
-  uint32_t mt[N] = {0};
-  uint16_t mti = 0;
-
  public:
   /*!
    * \brief MersenneTwister construct a new generator using the given seed
    */
-  MersenneTwister(uint32_t seed) { setSeed(seed); }
+  MersenneTwister(uint32_t seed);
+  ~MersenneTwister();
 
   /*!
    * \brief setSeed sets the underlying seed. The implementation will always return the same sequence of bytes for the
    * same seed.
    * \param seed the seed to be applied
    */
-  void setSeed(uint32_t seed) {
-    mt[0] = seed & 0xffffffff;
-    for (mti = 1; mti < N; mti++) mt[mti] = (69069 * mt[mti - 1]) & 0xffffffff;
-  }
+  void setSeed(uint32_t seed);
 
   /*!
    * \brief generateUnit generates the next atomic unit
    * \return a random distributed 32bit integer
    */
-  uint32_t generateUnit() {
-    uint32_t y;
-
-    if (mti >= N) {
-      uint32_t kk;
-
-      for (kk = 0; kk < N - M; kk++) {
-        y = (mt[kk] & UPPER_MASK) | (mt[kk + 1] & LOWER_MASK);
-        mt[kk] = mt[kk + M] ^ (y >> 1) ^ mag01[y & 0x1];
-      }
-
-      for (; kk < N - 1; kk++) {
-        y = (mt[kk] & UPPER_MASK) | (mt[kk + 1] & LOWER_MASK);
-        mt[kk] = mt[kk + (M - N)] ^ (y >> 1) ^ mag01[y & 0x1];
-      }
-
-      y = (mt[N - 1] & UPPER_MASK) | (mt[0] & LOWER_MASK);
-      mt[N - 1] = mt[M - 1] ^ (y >> 1) ^ mag01[y & 0x1];
-
-      mti = 0;
-    }
-
-    y = mt[mti++];
-    y ^= TEMPERING_SHIFT_U(y);
-    y ^= TEMPERING_SHIFT_S(y) & TEMPERING_MASK_B;
-    y ^= TEMPERING_SHIFT_T(y) & TEMPERING_MASK_C;
-    y ^= TEMPERING_SHIFT_L(y);
-
-    return y;
-  }
+  uint32_t generateUnit();
 
   /*!
    * \brief next generates a random value in the range [min, max]
@@ -109,27 +57,27 @@ class MersenneTwister {
    * \param max the maximum value to be generated
    * \return random value in [min, max]
    */
-  uint32_t next(uint32_t min, uint32_t max) {
-    uint32_t r = generateUnit();
-    double div = (double)(0xffffffff) / (double)(max - min);
-    return static_cast<uint32_t>(r / div) + min;
-  }
+  uint32_t next(uint32_t min, uint32_t max);
 
   /*!
    * \brief next creates a new random value in the range [0, count)
    * \param count the exclusive upper limit
    * \return random value in [0, count)
    */
-  uint32_t next(uint32_t count) { return next(0, count - 1); }
+  uint32_t next(uint32_t count);
 
   /*!
    * \brief nextBytes generates a sequence of random bytes
    * \param data the output buffer to store the sequence
    * \param length the amount of bytes to be generated
    */
-  void nextBytes(uint8_t* data, uint32_t length) {
-    for (uint32_t i = 0; i < length; i++) data[i] = (uint8_t)(generateUnit() / (0xffffffff / (uint32_t)0xff));
-  }
+  void nextBytes(uint8_t* data, uint32_t length);
+
+ private:
+  void fillBuffer();
+
+  struct _State;
+  std::unique_ptr<_State> m_State;
 };
 }  // namespace Crypto
 }  // namespace Xi
