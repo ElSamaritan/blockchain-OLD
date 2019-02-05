7fffb780cbad5774744290236b98b829334ab3d6
Michael Herwig <michael.herwig@hotmail.de>
2019-02-05 22:51:49 +0100

    [CORE] Xi:Error now exposes its variant error encoding

--- src/Xi-Core/source/Error.cpp
+++ src/Xi-Core/source/Error.cpp
@@ -27,19 +27,23 @@ Xi::Error::Error(std::exception_ptr e) : m_error{e} {}
 Xi::Error::Error(std::error_code ec) : m_error{ec} {}
 
 std::string Xi::Error::message() const {
-  if (m_error.type() == typeid(std::exception_ptr)) {
-    auto e = boost::get<std::exception_ptr>(m_error);
+  if (isException()) {
     try {
-      std::rethrow_exception(e);
+      std::rethrow_exception(exception());
     } catch (const std::exception& e) {
       return e.what();
     } catch (...) {
       return "Unknown type has been thrown.";
     }
-  } else if (m_error.type() == typeid(std::error_code)) {
-    auto ec = boost::get<std::error_code>(m_error);
-    return ec.message();
+  } else if (isErrorCode()) {
+    return errorCode().message();
   } else {
     return "Error wrapper contains unknown error type.";
   }
 }
+
+bool Xi::Error::isException() const { return m_error.type() == typeid(std::exception_ptr); }
+std::exception_ptr Xi::Error::exception() const { return boost::get<std::exception_ptr>(m_error); }
+
+bool Xi::Error::isErrorCode() const { return m_error.type() == typeid(std::error_code); }
+std::error_code Xi::Error::errorCode() const { return boost::get<std::error_code>(m_error); }
